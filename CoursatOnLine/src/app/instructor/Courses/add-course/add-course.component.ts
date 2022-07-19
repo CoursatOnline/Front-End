047aed93bdc3a8e1c03ef1d129684f78627843b8
Nada-Mamdouh <nada-mamdouh@outlook.com>
2022-07-19 17:30:12 +0200

    finishing part of add course component

--- CoursatOnLine/src/app/instructor/Courses/add-course/add-course.component.ts
+++ CoursatOnLine/src/app/instructor/Courses/add-course/add-course.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { CoursesService } from 'src/app/services/courses.service';
+import { CategoriesService } from 'src/app/services/categories.service';
+import { Category } from 'src/app/_models/category';
+import { Course } from 'src/app/_models/course';
 
 @Component({
   selector: 'app-add-course',
@@ -6,13 +10,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./add-course.component.css']
 })
 export class AddCourseComponent implements OnInit {
+  cats:Category|any = [];
+  course:Course|any = {};
+  public catBaseUrl:string = "https://localhost:7135/api/category/";
+  public courseBaseUrl:string = "https://localhost:7135/api/course/";
 
-  constructor() { }
+  constructor(private courseSer:CoursesService, private catSer:CategoriesService) { }
 
   ngOnInit(): void {
+    this.catSer.get(this.catBaseUrl+"getall").subscribe(
+      value => {this.cats = value;/*console.log(this.cats)*/},
+      error => {console.log(error)}
+    )
   }
   onCreate(value:any){
-    console.log(value);
+    this.course.name = value.crsname;
+    this.course.description = value.crsdescription;
+    this.course.price = value.crsprice;
+    this.course.ispaid = JSON.parse(value.crsispaid);
+    this.course.insid = value.crsins;
+
+    // this.courseSer.post(this.courseBaseUrl,this.course).subscribe
+
+    console.log(this.course);
   }
 
 }
--- CoursatOnLine/src/app/services/courses.service.ts
+++ CoursatOnLine/src/app/services/courses.service.ts
@@ -1,9 +1,18 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { Course } from '../_models/course';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CoursesService {
 
-  constructor() { }
+  constructor(private httpclient:HttpClient) { }
+  public get(url: string): Observable<Course|any>{
+    return this.httpclient.get(url);
+  }
+  public post(url: string,course:Course,options: any):Observable<Course | any>{
+    return this.httpclient.post(url,course);
+  }
 }
